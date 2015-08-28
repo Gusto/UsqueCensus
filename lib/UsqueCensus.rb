d3deaf74be69ddb8f04d606aded53334cc144552
Alex Rumbaugh <arumbaugh@raybeam.com>
2015-08-28 10:14:02 -0700

    Added rake tasks

--- lib/UsqueCensus.rb
+++ lib/UsqueCensus.rb
@@ -6,52 +6,64 @@ require 'yaml'
 
 
 module UsqueCensus
-    require 'UsqueCensus/railtie' if defined?(Rails)
-
-
+    class BackupTask < Rails::Railtie
+      rake_tasks do
+        Dir[File.join(File.dirname(__FILE__),'tasks/*.rake')].each { |f| load f }
+      end
+    end
 
     def self.initialize aws = nil, db = nil
-        if aws and db
-            settings = YAML::load(File.open(aws))
-            settings.merge!(YAML::load(File.open(db)))
-        else
-            settings = Settings
-            if aws
-                settings.merge!(YAML::load(File.open(aws)))
-            else
-                settings.merge!(YAML::load(File.open(db)))
-            end
-        end
+        load_aws aws
+        load_db db
+    end
 
-        puts(settings)
-        @@host = settings[:redshift][Rails.env][:host]
-        @@port = settings[:redshift][Rails.env][:port]
-        @@db = settings[:redshift][Rails.env][:database]
-        @@username = settings[:redshift][Rails.env][:username]
-        @@password = settings[:redshift][Rails.env][:password]
-        @@aws_access_key = settings[Rails.env][:aws][:aws_access_key_id]
-        @@aws_secret_key = settings[Rails.env][:aws][:aws_secret_key]
-        
+    def self.load_aws aws = nil
+        if aws
+            aws = YAML::load(File.open(aws))
+            @@aws_access_key = aws[Rails.env]['aws']['aws_access_key_id']
+            @@aws_secret_key = aws[Rails.env]['aws']['aws_secret_key']
+        else
+            @@aws_access_key = Settings[Rails.env][:aws][:aws_access_key_id]
+            @@aws_secret_key = Settings[Rails.env][:aws][:aws_secret_key]
+        end
+    end
+
+    def self.load_db db = nil
+        if db
+            db = YAML::load(File.open(db))
+            @@host = db['redshift'][Rails.env]['host']
+            @@port = db['redshift'][Rails.env]['port']
+            @@db = db['redshift'][Rails.env]['database']
+            @@username = db['redshift'][Rails.env]['username']
+            @@password = db['redshift'][Rails.env]['password']
+        else
+            @@host = settings[:redshift][Rails.env][:host]
+            @@port = settings[:redshift][Rails.env][:port]
+            @@db = settings[:redshift][Rails.env][:database]
+            @@username = settings[:redshift][Rails.env][:username]
+            @@password = settings[:redshift][Rails.env][:password]
+        end
         @@connection = PG::Connection.new({
             :host       => @@host,
             :port       => @@port,
             :dbname     => @@db,
             :user       => @@username,
             :password   => @@password
-            })
+        })
     end
 
 
-    def self.download_data
+
+    def self.download_data file
         #data looks like this...
         #   [["P0300001","state","county"],
         #   ["54116","01","001"],
         #   ["179958","01","003"],
         #   ["24264","01","005"],
         #So we need to convert to csv format for storage
-        puts(fields)
+        WebMock.allow_net_connect!
         url = "http://api.census.gov/data/2010/sf1?for=county:*&get=P0300001"
-        open('census.csv', 'wb') do |file|
+        open(file, 'wb') do |file|
           open(url).each do |line|
             #remove the []'s 
             line = /".*"/.match(line).to_s + "\n"
@@ -61,13 +73,15 @@ module UsqueCensus
         end
     end
 
-    def self.upload_to_S3 file, bucket='bi-testing'
-        FilePather.copy file, "s3://#{bucket}/census.csv"
+    def self.upload_to_S3 file, bucket
+        WebMock.allow_net_connect!
+        destname = Pathname(file).basename
+        FilePather.copy file, "s3://#{bucket}/#{destname}"
     end
 
-    def self.create_table_redshift
+    def self.create_table_redshift tablename
         sql = <<-sql
-        CREATE TABLE IF NOT EXISTS rawcensus(
+        CREATE TABLE IF NOT EXISTS #{tablename}(
            POP INTEGER          NOT NULL,
            STATE INTEGER           NOT NULL,
            COUNTY INTEGER         NOT NULL,
@@ -77,9 +91,9 @@ module UsqueCensus
         @@connection.exec(sql)
     end
 
-    def self.load_into_redshift bucket='bi-testing'
+    def self.load_into_redshift filename, bucket, tablename
         sql = <<-sql
-        copy rawcensus from 's3://#{bucket}/census.csv'
+        copy #{tablename} from 's3://#{bucket}/#{filename}'
         credentials 'aws_access_key_id=#{@@aws_access_key};aws_secret_access_key=#{@@aws_secret_key}'
         DELIMITER AS ','
         IGNOREHEADER 1
@@ -88,16 +102,15 @@ module UsqueCensus
         @@connection.exec(sql)
     end
 
-    def self.run
-        initialize
+    def self.run file, bucket, tablename
         puts ("Downloading..")
-        download_data
+        download_data file
         puts ("Uploading to S3...")
-        upload_to_S3 'census.csv'
+        upload_to_S3 file, bucket
         puts ("Creating Redshift Table...")
-        create_table_redshift
+        create_table_redshift tablename
         puts ("Loading data into Redshift...")
-        load_into_redshift
+        load_into_redshift Pathname(file).basename, bucket, tablename
     end
 
 
--- lib/tasks/census.rake
+++ lib/tasks/census.rake
@@ -1,18 +1,34 @@
 namespace :usquecensus do
-    desc('Downloads the census data')
-    task :download_files do
-        puts("Downloading data")
-        UsqueCensus.download_data
+    desc('Download census data')
+    task :download_data, [:file] do |t, args|
+        puts("Downloading data...")
+        UsqueCensus.download_data args[:file]
     end
 
-    desc('Test Task  uc:: lib/taks/test.rake')
-    task :upload_to_S3 do
-        puts("Running !!!!!!!!!!!!!!!!!!!")
+    desc('Upload census data to S3')
+    task :upload_to_S3, [:file, :bucket] do |t, args|
+        puts("Uploading to S3...")
+        UsqueCensus.upload_to_S3 args[:file], args[:bucket]
     end
 
-    desc('Test Task  uc:: lib/taks/test.rake')
-    task :z_test_task do
-        puts("Running !!!!!!!!!!!!!!!!!!!")
+    desc('Creates a redshift table with population, state, and county columns')
+    task :create_table_redshift, [:tablename] do |t, args|
+        puts("Creating redshift table...")
+        UsqueCensus.initialize(nil, Rails.root+'config/redshift.yml')
+        UsqueCensus.create_table_redshift args[:tablename]
+    end
+
+    desc('Loads data from S3 into redshift')
+    task :load_into_redshift, [:file, :bucket, :tablename] do |t, args|
+        puts("Loading data into redshift...")
+        UsqueCensus.initialize(nil, Rails.root+'config/redshift.yml')
+        UsqueCensus.load_into_redshift args[:file], args[:bucket], args[:tablename]
+    end
+
+    desc('Runs the entire census file flow')
+    task :run_all, [:file, :bucket, :tablename] do |t, args|
+        UsqueCensus.initialize(nil, Rails.root+'config/redshift.yml')
+        UsqueCensus.run(args[:file],args[:bucket],args[:tablename])
     end
 
 end
